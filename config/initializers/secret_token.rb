ec7e7ab91f61ee603a138ae20d274e1e5414506a
Mark Mulder <markmulder@gmail.com>
2010-09-18 17:42:39 +0200

    Rails 3 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PluploadExample::Application.config.secret_token = '3088a0ad1d86ed68365db1f34eb3327e39d89e9c66da3c79eaf22953227df5dd0f6b93acde7303f99dce66c94b09bf4f23eff954daa844cdd188cdb876a8bf2f'
